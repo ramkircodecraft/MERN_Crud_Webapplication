6e0fe411857b3abb3a50d249c187a27d44461683
ramkircodecraft <ramkirider@gmail.com>
2024-10-18 14:23:58 +0530

    mongodb atlas connection trail

--- server/models/users.js
+++ server/models/users.js
@@ -1,10 +1,19 @@
-const mongoose = require('mongoose')
+/* const mongoose = require('mongoose') */
 
-const UserSchema = new mongoose.Schema({
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://<username>:<password>@cluster0.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('Connected to MongoDB Atlas'))
+.catch((err) => console.log('Error connecting to MongoDB Atlas:', err));
+
+/* const UserSchema = new mongoose.Schema({
     name: String,
     email:String,
     age:Number
 })
 
 const UserModel = mongoose.model("users",UserSchema)
-module.exports = UserModel
+module.exports = UserModel */
